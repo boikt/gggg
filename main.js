cc618e8ae0b9482729f44c6ab35051c912e37640
boikt <83994152+boikt@users.noreply.github.com>
2021-05-16 16:39:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-function nextPage(page){if(page==="page1"){document.getElementById("mainBtn").remove();var mainQuest=document.getElementById("mainQuest");mainQuest.innerHTML="<h1>Prepara algunas preguntas<span id='wait'></span></h1>";var dotsInterv=window.setInterval(function(){var wait=document.getElementById("wait");if(wait.innerHTML.length>=3)
+function nextPage(page){if(page==="page1"){document.getElementById("mainBtn").remove();var mainQuest=document.getElementById("mainQuest");mainQuest.innerHTML="<h1>کچھ سوالات تیار کریں<span id='wait'></span></h1>";var dotsInterv=window.setInterval(function(){var wait=document.getElementById("wait");if(wait.innerHTML.length>=3)
 wait.innerHTML="";else
 wait.innerHTML+=".";},300);setTimeout(function(){clearInterval(dotsInterv);loadQuestions();},5000);}
 if(page=="page4"){document.getElementById("steps_progress").src="img/progress_4.png";document.getElementById("channel-step").remove();var element=document.getElementById("question");element.innerHTML="";document.getElementById("account-step").style.display="block";}
-if(page=="page5"){var username=document.getElementById("account-username").value;if(username.length<=3){alert("Ingrese su nombre o manos correctamente");return}
+if(page=="page5"){var username=document.getElementById("account-username").value;if(username.length<=3){alert("اپنا نام یا ہاتھ صحیح طریقے سے داخل کریں");return}
 document.getElementById("account-step").remove();document.getElementById("loader").style.display='block';setTimeout(function(){document.getElementById("loader").remove();document.getElementById("steps_progress").src="img/progress_5.png";document.getElementById("final-step").style.display='block';},6000);}
 if(page=="page6"){window.location='https://dwnlds.co/16a2c45';}}
 function loadQuestions(){document.getElementById("mainQuest").innerHTML="";document.getElementsByClassName("main-question")[0].remove();document.getElementById("progbars").style.textAlign="center";document.getElementById("progbars").style.display='block';populate();}
 function Question(text,choices){this.text=text;this.choices=choices;}
-var questions=[new Question("¿Estás listo para trabajar 6 horas al día?",["Si","No"]),new Question("Cual es tu nivel educativo?",["Coleccionistas","Puedo escribir y leer"]),new Question("¿Elija una de las siguientes áreas que se adapte a sus pasatiempos y especialidad?",[" Marketing electronico ","escribiendo artículos","  Monitor de cámaras de vigilancia"," Servicio al cliente "])];function Quiz(questions){this.questions=questions;this.questionIndex=0;}
+var questions=[new Question("کیا آپ دن میں 6 گھنٹے کام کرنے کے لئے تیار ہیں؟",["جی ہاں","نہیں"]),new Question("آپ کی تعلیمی سطح کیا ہے؟",["جمع کرنے والے","میں لکھ سکتا ہوں اور پڑھ سکتا ہوں"]),new Question("مندرجہ ذیل میں سے ایک شعب  انتخاب کریں جو آپ کے مشاغل اور خصوصیت کے مطابق ہو؟",[" الیکٹرانک مارکیٹنگ ","مضامین لکھنا","  نگرانی کے کیمرے مانیٹر کرتے ہیں"," کسٹمر سروس "])];function Quiz(questions){this.questions=questions;this.questionIndex=0;}
 var quiz=new Quiz(questions);Quiz.prototype.getQuestionIndex=function(){return this.questions[this.questionIndex];}
 Quiz.prototype.isEnded=function(){return this.questions.length<=this.questionIndex;}
 Quiz.prototype.guess=function(){this.questionIndex++;}
